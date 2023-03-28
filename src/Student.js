0c8ae624af371d8f18381f8e21347ac34f76f3cc
Arvind <thakurarvindkr10@gmail.com>
2023-03-28 14:53:39 +0530

    I have added class based props whether you declare a component function or a class it must never modify it's own props All react component must be act like pure functions with respect to thier props

--- src/Student.js
+++ src/Student.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Student = (props) => {
-  return (
-    <div>
-      <h1>Hello your name {props.name}</h1>
-      <h1>Your roll : {props.roll}</h1>
-    </div>
-  )
+// functional based props
+// const Student = (props) => {
+//   return (
+//     <div>
+//       <h1>Hello your name {props.name}</h1>
+//       <h1>Your roll : {props.roll}</h1>
+//     </div>
+//   )
+// }
+
+// class based props
+class Student extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello your name {this.props.name}</h1>
+        <h1>Your roll : {this.props.roll}</h1>
+      </div>
+    );
+  }
 }
 
 export default Student;
