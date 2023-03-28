b9e9eaf2691bcc105a1dcde65faa4cc24466a0be
Arvind <thakurarvindkr10@gmail.com>
2023-03-28 13:13:10 +0530

    Composing components can refer to other components in thier output. this is let us use the same component abstraction for any level of details

--- src/Student.js
+++ src/Student.js
@@ -2,8 +2,7 @@ import React from "react";
 
 const Student = (props) => {
   return <div>
-    <h1>Hello {props.name} !!!!!!!</h1>
-    <h1>Hello Shayam !!!!!!!</h1>
+    <h1>Hello {props.name} !!!!!!!</h1> 
   </div>;
 };
 
