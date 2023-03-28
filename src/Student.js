c25baa0ed82881707372a06383c7eefedd0c6b2f
Arvind <thakurarvindkr10@gmail.com>
2023-03-28 21:33:04 +0530

    added event handler and update the setState

--- src/Student.js
+++ src/Student.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const Student = (props) => {
-  function handleEvent(e){
-    e.preventDefault();
-    console.log('button clicked')
+export class Student extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Viru",
+      roll: this.props.roll,
+    };
   }
-  return (
-    <div>
-      <h1>Your name is Arvind and roll no is {props.roll}</h1>
-      <button onClick={handleEvent}>Click Me</button>
-      <a href='https://www.google.com' onClick={handleEvent}>Click Me</a>
-    </div>
-  )
+
+  handleEvent = () => {
+    this.setState({ name: "Jai" , roll:102});
+    console.log('button clicked ', this)
+  };
+  // handleEvent = () => {
+  //   this.setState((state, props) => {
+      // console.log(state)
+      // console.log(props)
+      // state.name = 'Jai'
+      // state.roll = 102
+    // });
+    // console.log('button clicked ', this)
+  // };
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello {this.state.name} and your roll number is {this.state.roll}
+        </h1>
+        <button onClick={this.handleEvent}>Update Name</button>
+      </div>
+    );
+  }
 }
 
 export default Student;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Student from './Student';
+import Student from './Student'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Student roll={1819510002}/>)
-
+ReactDOM.createRoot(document.getElementById('root')).render(<Student roll={101}/>)
