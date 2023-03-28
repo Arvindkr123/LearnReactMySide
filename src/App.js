b744f24434e68824661e6b345ae3bcc377f3ea6c
Arvind <thakurarvindkr10@gmail.com>
2023-03-28 13:45:09 +0530

    I have added some concept related JSX in App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import Student from "./Student";
-class App extends Component {
-  render() {
-    return <div>
-      <Student name={"Arvind Kumar"}/>
-      <Student name={"SJS Kumar"}/>
-      <Student name={"APK Kumar"}/>
-      <Student name={"AKS Kumar"}/>
-    </div>;
+function App() {
+  const ele = 'Arvind Kumar'
+  const obj = {
+    firstName :'Ram Kumar'
   }
+
+  function show(){
+    console.log('I\'m show function')
+  }
+  return (
+    <div>
+      <h1>Name is {ele}</h1>
+      <h1>10 + 20 = {10 + 20}</h1>
+      <h1>obj firstName {obj.firstName}</h1>
+      <h1>{show()}</h1>
+    </div>
+  );
 }
+
 export default App;
