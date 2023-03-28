a9f8fa2c15e641baa12ca72d5790ce55d00b1e86
Arvind <thakurarvindkr10@gmail.com>
2023-03-28 11:01:49 +0530

    I have created class componet which require to extends class component and this class must implement a render function which returns a React component to rendered similar to return value functional component In a class based component props are accesible via this.props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello Arvind Kumar!!!</h1>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return <div>
+      <h1>Hello Arvind !!!!!!!!</h1>
+    </div>;
+  }
 }
-
 export default App;
