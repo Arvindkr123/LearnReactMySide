f29b51da2bf1b45463fa20ede85914fb06df6d22
Arvind <thakurarvindkr10@gmail.com>
2023-03-29 20:19:19 +0530

    20##ListInReact

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import React, { Component } from "react";
 export default class App extends Component {
   render() {
     const arr = [10, 20, 30, 40, 50];
+    const newArray = arr.map((num) => {
+      return <li>{num*2}</li>;
+    });
     return (
       <>
-        <ul>
-          <li>{arr[0] * 2}</li>
-          <li>{arr[1] * 2}</li>
-          <li>{arr[2] * 2}</li>
-          <li>{arr[3] * 2}</li>
-          <li>{arr[4] * 2}</li>
-        </ul>
+        <ul>{newArray}</ul>
       </>
     );
   }
