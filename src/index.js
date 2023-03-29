77998766fc2752b36cc8696c531524d0ec0cb939
Arvind <thakurarvindkr10@gmail.com>
2023-03-30 03:30:02 +0530

    pass array as props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+let arr = [10,20,30,40];
+ReactDOM.render(<App arr={arr}/>, document.getElementById("root"));
