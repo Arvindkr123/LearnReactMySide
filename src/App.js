a68b66a10a04535c48f8c11237ca42397d648414
Arvind <thakurarvindkr10@gmail.com>
2023-03-30 03:29:32 +0530

    Key in the map

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class App extends Component {
-  state = {
-    users: [
-      { id: 101, name: "Arvind", password: "12323dfdsdd" },
-      { id: 102, name: "Ram", password: "12323dfdsdd" },
-      { id: 103, name: "Allu Arjun", password: "12323dfdsdd" },
-    ],
-    isLoggedIn: false,
-  };
   render() {
-    // const newUser = this.state.users.map((user)=>{
-    //   return <><h1><li>{user.id} {user.name} {user.password}</li></h1></>
-    // })
     return (
       <>
-        <ul>
-          {this.state.users.map((user) => {
-            return (
-              <li key={user.id}>
-                Id {user.id} Name {user.name} Password {user.password}
-              </li>
-            );
-          })}
-        </ul>
+        {this.props.arr.map((num, i)=>{
+          return <li key={i}>{num}</li>
+        })}
       </>
-    );
+    )
   }
 }
