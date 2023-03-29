99bf73b1da2f6bebbfec96830084000170ee8c07
Arvind <thakurarvindkr10@gmail.com>
2023-03-30 04:06:52 +0530

    Change the background color when i click button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  state = {
+    change: false,
+  };
+  handleClick = () => {
+    this.setState({
+      change: !this.state.change,
+    });
+  };
+
   render() {
-    const textc = {
-      color: "blue", 
+    const btnstyle = {
+      color: "white",
+      backgroundColor: "black",
+      border: "none",
+      padding: "10px",
+      margin: "10px",
+      cursor: "pointer",
     };
-    const texts = {
-      fontSize: "80px",
-    };
+    if (this.state.change === true) {
+      btnstyle.backgroundColor = "orange";
+    }
     return (
       <>
-        <h1 style={{ ...textc, ...texts, ...{backgroundColor:"yellow"} }}>Hello App</h1>
+        <button style={btnstyle} onClick={this.handleClick}>
+          Click me
+        </button>
       </>
     );
   }
