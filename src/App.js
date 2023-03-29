9fab1a7b21432c07e9a736c9230901fdcf8a49ef
Arvind <thakurarvindkr10@gmail.com>
2023-03-29 21:45:10 +0530

    added arr props and map

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    const arr = [10, 20, 30, 40, 50];
-    const newArray = arr.map((num) => {
-      return <li>{num*2}</li>;
-    });
+    const arr = this.props.arr;
     return (
       <>
-        <ul>{newArray}</ul>
+        <ul>
+          {arr.map((num) => {
+            return <li>{num}</li>;
+          })}
+        </ul>
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const arr = [10, 20, 30, 40, 50];
+ReactDOM.createRoot(document.getElementById("root")).render(<App arr={arr}/>);
