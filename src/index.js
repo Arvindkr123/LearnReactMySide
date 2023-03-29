e1617cc7753ad6562efd1a6bbff8f310715ebacf
Arvind <thakurarvindkr10@gmail.com>
2023-03-29 22:50:52 +0530

    modify

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const arr = [10, 20, 30, 40, 50];
-ReactDOM.createRoot(document.getElementById("root")).render(<App arr={arr}/>);
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
