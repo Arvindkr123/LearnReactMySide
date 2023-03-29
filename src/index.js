f2a9fef4b04bc212e63b954099974954bbf8e6bc
Arvind <thakurarvindkr10@gmail.com>
2023-03-29 09:10:27 +0530

    added some info check this out

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; 
+import Student from "./Student";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App name="Arvind"/>);
+ReactDOM.createRoot(document.getElementById("root")).render(<Student/>);
