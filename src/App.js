b641ecc086d69a0353dbe243ca25aa3b14312951
Arvind <thakurarvindkr10@gmail.com>
2023-03-30 03:41:24 +0530

    Inline Style in the react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
+    const styleBtn = {
+      backgroundColor: 'blue',
+      color: 'white',
+      padding: '10px',
+      margin: '10px'
+    }
     return (
       <>
-        {this.props.arr.map((num, i)=>{
-          return <li key={i}>{num}</li>
-        })}
+        <h1>Hello Styling in React</h1>
+        <button style={styleBtn}>click me</button>
       </>
     )
   }
