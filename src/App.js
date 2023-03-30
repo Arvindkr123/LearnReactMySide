a16928f4877175596d3ee3e72ea586479e6a7f5a
Arvind <thakurarvindkr10@gmail.com>
2023-03-30 11:46:24 +0530

    confilicting while using external css when if you give the same name class then conflict occurs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from "react";
-
+import "./App.css";
+import User from "./User";
 export default class App extends Component {
-  state = {
-    change: false,
-  };
-  handleClick = () => {
-    this.setState({
-      change: !this.state.change,
-    });
-  };
-
   render() {
-    const btnstyle = {
-      color: "white",
-      backgroundColor: "black",
-      border: "none",
-      padding: "10px",
-      margin: "10px",
-      cursor: "pointer",
-    };
-    if (this.state.change === true) {
-      btnstyle.backgroundColor = "orange";
-    }
     return (
       <>
-        <button style={btnstyle} onClick={this.handleClick}>
-          Click me
-        </button>
+        <h1 className="txt">Hello App</h1>
+        <User/>
       </>
     );
   }
---
+++ src/User.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./User.css";
+
+export default class User extends Component {
+  render() {
+    return (
+      <>
+        <h2 className="txt1">Hello User</h2>
+      </>
+    );
+  }
+}
