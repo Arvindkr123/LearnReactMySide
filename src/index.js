216709d6ed662d41af57036ae3f553082eb6653b
Arvind <thakurarvindkr10@gmail.com>
2023-05-06 07:18:06 +0530

    added new way changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
 // let element = <h1>Welcome Back Arvind</h1> // JSX Code Browser doesn't understand this code it's understand only JS code
@@ -9,4 +9,8 @@ import App from "./App";
 // if component we have used just like html elemenet
 // ReactDOM.render(<App></App>, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
